f444395e5f46d40628a4dc221c452fd36cb0b7f4
Jullian Mercier <31648126+jullianm@users.noreply.github.com>
2023-03-11 08:47:47 +0100

    chore(packages): add Core and App local packages

---
+++ App/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "App",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "App",
+            targets: ["App"]),
+    ],
+    dependencies: [
+        .package(path: "../Core")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "App",
+            dependencies: [],
+            path: "Sources",
+            resources: [
+                .process("Resources/Xib")
+            ]
+        ),
+        .testTarget(
+            name: "Tests",
+            dependencies: [
+                "App",
+                "Core"
+            ],
+            path: "Tests")
+    ]
+)
